7b71a8f7c785f690afc307a3eeb1fcce3cf2f723
KINGHacker <sinashabani08@gmail.com>
2026-06-05 19:45:31 +0330

    Add files via upload

---
+++ mune.py
@@ -0,0 +1,19 @@
+menu = """
+  	 <---menu-->
+__________________________________
+|New folder [01]		 |
+|				 |
+|IP your[02]			 |
+|				 |
+|storage for termux[03]		 |
+|				 |
+|install script iA KINGHacker[04]|
+|				 |
+|Kali nethunter[05]		 |
+|				 |
+|SDcard storage[06]		 |
+|				 |
+|exit[07]			 |
+_________________________________|
+"""
+print(menu)
